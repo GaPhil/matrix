739f408a3d6b2a1199c37ed07f12c2efa9c641f6
GaPhil <gaphil.io@gmail.com>
2018-09-07 14:56:19 +0200

    Finish transpose

--- transpose.c
+++ transpose.c
@@ -1,19 +1,46 @@
 #include <stdio.h>
 
-int main() {
-   int rows;
-   int columns;
-   printf("Enter the number of rows:");
-   scanf("%d", &rows);
-   printf("Enter the number of columns:");
-   scanf("%d", &columns);
-   printf("Rows: %d and Columnds: %d\n", rows, columns);
-
-   int matrix[rows][columns];
-   for(int i = 1; i <= rows; i++) {
-      for(int j = 1; j <= columns; j++) {
-         printf("Enter the number at position %d %d ", i, j);
+void read(int rows, int columns, int matrix[rows][columns]) {
+   for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < columns; j++) {
+         printf("Enter the number at position %d %d: ", i + 1, j + 1);
          scanf("%d:", &matrix[i][j]);
       }
    }
+}
+
+void print(int rows, int columns, int matrix[rows][columns]) {
+   for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < columns; j++) {
+         printf("%d ", matrix[i][j]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+}
+
+void transpose(int rows, int columns, int matrix[rows][columns], int transposed_matrix[columns][rows]) {
+   for(int j = 0; j < columns; j++) {
+      for(int i = 0; i < rows; i++) {
+         transposed_matrix[j][i] = matrix[i][j];
+      }
+   }
+}
+
+int main() {
+   int rows;
+   int columns;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   printf("Enter the number of columns: ");
+   scanf("%d", &columns);
+   printf("Rows: %d and Columns: %d\n", rows, columns);
+
+   int matrix[rows][columns];
+   read(rows, columns, matrix);
+   print(rows, columns, matrix);
+
+   int transposed_matrix[columns][rows]; 
+   transpose(rows, columns, matrix, transposed_matrix);
+   print(columns, rows, transposed_matrix);
 }
