bc429b9d6982157b0829f84a91839584c064bb88
GaPhil <gaphil.io@gmail.com>
2018-09-30 20:43:05 +0200

    Update

--- multiplication.c
+++ multiplication.c
@@ -19,29 +19,48 @@ void print(int rows, int columns, int matrix[rows][columns]) {
    printf("\n");
 }
 
-void multiply(int matrix_a[rows_a][columns_a], int matrix_b[rows_b][columns_b], int matrix_result[rows_a][columns_b]) {
-   int temp;
-   for(int i = 0; i < rows_a; i++) {
-      for(int j = 0; j < columns_b; j++) {
-         // iteraate through the columns of a and add to the rows of b
-         for(int k = 0; k < rows_a; k++) {
-            temp = matrix_a[i][k] + matrix_b[k][j];
+void multiply(int rows, int columns, int rows_b, int columns_a, int matrix_a[rows][columns_a], int matrix_b[rows_b][columns], int matrix_result[rows][columns]) {
+   for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < columns; j++) {
+         matrix_result[i][j] = 0;
+         for(int k = 0; k < columns_a; k++) {
+            matrix_result[i][j] += matrix_a[i][k] * matrix_b[k][j];
          }
-         matrix_result[i][j] = temp;
       }
    }
 }
 
 int main() {
-   int rows;
-   int columns;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   printf("Enter the number of columns: ");
-   scanf("%d", &columns);
-   printf("Rows: %d and Columns: %d\n", rows, columns);
+   int rows_a, rows_b;
+   int columns_a, columns_b;
+   printf("Enter the number of rows of matrix A: ");
+   scanf("%d", &rows_a);
+   printf("Enter the number of columns of matrix A: ");
+   scanf("%d", &columns_a);
+   printf("Rows: %d and Columns: %d\n", rows_a, columns_a);
 
-   int matrix[rows][columns];
-   read(rows, columns, matrix);
-   print(rows, columns, matrix);
+   int matrix_a[rows_a][columns_a];
+   read(rows_a, columns_a, matrix_a);
+   print(rows_a, columns_a, matrix_a);
 
+   printf("Enter the number of rows of matrix B: ");
+   scanf("%d", &rows_b);
+   printf("Enter the number of columns of matrix B: ");
+   scanf("%d", &columns_b);
+   printf("Rows: %d and Columns: %d\n", rows_b, columns_b);
+
+   if(rows_a != columns_a && rows_b != columns_a) {
+      printf("\nERROR: Matrix dimensions incorrect!\n");
+      return 0;
+   }
+
+   int matrix_b[rows_b][columns_b];
+   read(rows_b, columns_b, matrix_b);
+   print(rows_b, columns_b, matrix_b);
+
+   int matrix_result[rows_a][columns_b];
+   multiply(rows_a, columns_b, rows_b, columns_a, matrix_a, matrix_b, matrix_result);
+   printf("Resulting matrix:\n");
+   printf("Rows: %d and Columns: %d\n", rows_a, columns_b);
+   print(rows_a, columns_b, matrix_result);
+}
